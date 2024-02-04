360f7615bbb0cf280b986a3c3b26f7fd084eeb53
Viktar Tsialitsyn <vtsialitsyn@playtika.com>
2024-02-04 22:39:42 +0100

    add Cocoapods support

---
+++ Example/Concurrency/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Concurrency
+//
+//  Created by Viktar Tsialitsyn on 02/04/2024.
+//  Copyright (c) 2024 Viktar Tsialitsyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
